ac647ca890ccd13332690411a606bdb8b58865fa
Paul <pjwu89@gmail.com>
2012-06-10 14:52:54 -0500

    fixed delete on item and removed pg gem

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -79,7 +79,7 @@ class ItemsController < ApplicationController
     @item.destroy
 
     respond_to do |format|
-      format.html { redirect_to line_items_url }
+      format.html { redirect_to list_items_url }
       format.json { head :no_content }
     end
   end
